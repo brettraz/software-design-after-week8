21c6f9f03351dc6a419d788459791e15ca7c2967
brettraz <brett.rasmussen@kellogg.northwestern.edu>
2020-02-26 23:26:00 +0000

    create DB

--- createdb.rb
+++ createdb.rb
@@ -4,31 +4,44 @@ DB = Sequel.connect "sqlite://#{Dir.pwd}/development.sqlite3"          #
 ########################################################################  
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :items do
   primary_key :id
-  String :title
+  Boolean :status_available
+  String :item_type
+  String :item_name
+  String :condition
   String :description, text: true
-  String :date
-  String :location
+  String :price
+  
 end
-DB.create_table! :rsvps do
+
+  
+ 
+
+DB.create_table! :customer_input do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
-  String :comments, text: true
+  foreign_key :item_id
+  String :customer_name
+  String :customer_phone_number
+  String :customer_e_mail
+  String :desired_pick_up_times, text: true
 end
-
 # Insert initial (seed) data
-events_table = DB.from(:events)
+items_table = DB.from(:items)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+items_table.insert(item_name: "child gates", 
+                    description: "3 sets of gates to hook up in your home to protect your young kids from stairs and keep pets on one level.  Child proof to open and close.",
+                    item_type: "child gates",
+                    condition: "like new",
+                    price: "$30",
+                    status_available: 1)
+                    
+
+                    items_table.insert(item_name: "Child Crib and bed", 
+                    description: "3 in 1 crib, toddler bed, bed with gates.  This will accomodate your child's sleeping needs from infant to adolescent all in this convertible set.",
+                    item_type: "crib and child's bed",
+                    condition: "Used but stil good condition",
+                    price: "$85",
+                    status_available: 1)
+                    
